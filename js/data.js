2b91d618d724812e3cf7d7e7ad09c50c48cf5ff3
shanemmay <shanemarvinmay@hotmail.com>
2018-10-04 22:37:05 -0500

    init commit

---
+++ js/data.js
@@ -0,0 +1,9 @@
+let data = 
+{
+    1:10,
+    2:20,
+    3:30
+};
+
+ //export default {data};
+ //module.exports = data;
---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log(data);
+
+
+
+/*
+let data = require('./data');
+
+for (entry in data)
+{
+    console.log(entry);
+}
+*/
